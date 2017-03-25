caa2a33611c9a7a4eb98c59b99b09d2627712185
Linshuo Li <lilinshuo1110@gmail.com>
2017-03-25 19:54:59 -0400

    test

---
+++ py/app.py
@@ -0,0 +1,53 @@
+# from flask import Flask
+# from flask_mysqldb import MySQL
+#
+# mysql = MySQL()
+#
+# app = Flask(__name__)
+#
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+# app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# mysql.init_app(app)
+#
+#
+# @app.route('/')
+# def hello():
+#     return 'Welcome to Python Flask App!'
+#
+#
+# if __name__ == '__main__':
+#     app.run()
+# def main():
+#     return render_template('index.html')
+#
+# def get_db():
+#     """Opens a new database connection if there is none yet for the
+#     current application context.
+#     """
+#     if not hasattr(g, 'sqlite_db'):
+#         g.sqlite_db = connect_db()
+#     return g.sqlite_db
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
