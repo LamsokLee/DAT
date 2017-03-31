c8b854d44f3b0b2400ce280fc51de4045d011a3e
mtian2 <mytian91@gmail.com>
2017-03-31 00:47:03 -0400

    kc2

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import *
+
+from main import putDB
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def worksheet1():
+    return current_app.send_static_file('input.html')
+
+
+@app.route('/worksheet2', methods=['POST'])
+def worksheet2():
+    # print(request.form.get('comment1'))
+    putDB(request.form.get('comment1'))
+    return current_app.send_static_file('1.html')
+#
+#
+if __name__ == '__main__':
+    app.run()
+# def main():
+#     return render_template('index.html')
+#
+# def get_db():
+#     """Opens a new database connection if there is none yet for the
+#     current application context.
+#     """
+#     if not hasattr(g, 'sqlite_db'):
+#         g.sqlite_db = connect_db()
+#     return g.sqlite_db
+
---
+++ main.py
@@ -0,0 +1,31 @@
+# import db as db
+from flask import *
+from model import *
+
+
+def putDB(comments):
+
+    admin = answers(comments)
+    db.session.add(admin)
+    db.session.commit()
+
+# putDB('lllllll')
+
+
+
+
+
+# conn = MySQLdb.connect(host= "104.131.52.198.",
+#                   user="root",
+#                   passwd="84519851",
+#                   db="DP")
+#
+#
+# cursor = db.cursor()
+# sql = "INSERT INTO answers(ans1) VALUES ('jadhfiuaeh')"
+# cursor.execute(sql)
+# db.commit()
+# db.close()
+
+
+
