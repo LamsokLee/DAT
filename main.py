f8a93d06ee14b90af161405ca8d83f298b3e1ab4
Linshuo Li <lilinshuo1110@gmail.com>
2017-04-03 14:19:11 -0400

    fixed css ref

--- main.py
+++ main.py
@@ -9,24 +9,3 @@ def putDB(comments):
     admin = answers(comments)
     db.session.add(admin)
     db.session.commit()
-
-# putDB('lllllll')
-
-
-
-
-
-# conn = MySQLdb.connect(host= "104.131.52.198.",
-#                   user="root",
-#                   passwd="84519851",
-#                   db="DP")
-#
-#
-# cursor = db.cursor()
-# sql = "INSERT INTO answers(ans1) VALUES ('jadhfiuaeh')"
-# cursor.execute(sql)
-# db.commit()
-# db.close()
-
-
-
