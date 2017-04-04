ae8d2e52ca2ce21728a6731f8ad8b3ee4cc8a38d
Linshuo Li <lilinshuo1110@gmail.com>
2017-04-04 13:27:11 -0400

    Fixed the img link

--- __init__.py
+++ __init__.py
@@ -4,8 +4,80 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template("1.html", title = "this is the test")
+    return render_template("home.html")
+
+@app.route('/1')
+def page_1():
+    return render_template("1.html")
+
+@app.route('/2')
+def page_2():
+    return render_template("2.html")
+
+@app.route('/3')
+def page_3():
+    return render_template("3.html")
+
+@app.route('/4')
+def page_4():
+    return render_template("4.html")
+
+@app.route('/5')
+def page_5():
+    return render_template("5.html")
+
+@app.route('/6')
+def page_6():
+    return render_template("6.html")
+
+@app.route('/7')
+def page_7():
+    return render_template("7.html")
+
+@app.route('/8')
+def page_8():
+    return render_template("8.html")
+
+@app.route('/9')
+def page_9():
+    return render_template("9.html")
+
+@app.route('/10')
+def page_10():
+    return render_template("10.html")
+
+@app.route('/11')
+def page_11():
+    return render_template("11.html")
+
+@app.route('/12')
+def page_12():
+    return render_template("12.html")
+
+@app.route('/13')
+def page_13():
+    return render_template("13.html")
+
+@app.route('/14')
+def page_14():
+    return render_template("14.html")
+
+@app.route('/15')
+def page_15():
+    return render_template("15.html")
+
+@app.route('/16')
+def page_16():
+    return render_template("16.html")
+
+@app.route('/17')
+def page_17():
+    return render_template("17.html")
+
+@app.route('/18')
+def page_18():
+    return render_template("18.html")
+
 
 if __name__ == "__main__":
     app.run()
-    # sddf
--- app.py
+++ app.py
@@ -6,21 +6,24 @@ from main import *
 from model import app
 
 
-@app.route('/')
-def worksheet1():
-    return render_template('18.html')
+# @app.route('/')
+# def worksheet1():
+#     return render_template('18.html')
 
-
-@app.route('/worksheet2', methods=['POST'])
-def worksheet2():
-    # print(request.form.get('comment1'))
-    putDB(request.form.get('comment1'),request.form.get('comment2'),
-          request.form.get('comment3'),request.form.get('comment4'),
-          request.form.get('comment5'),request.form.get('comment6'),
-          request.form.get('comment7'),request.form.get('comment8'),
-          request.form.get('comment9'),request.form.get('comment10'))
-
+@app.route('/preview')
+def preview():
     return render_template('preview.html')
+
+# @app.route('/preview', methods=['POST'])
+# def preview():
+#     # print(request.form.get('comment1'))
+#     putDB(request.form.get('comment1'),request.form.get('comment2'),
+#           request.form.get('comment3'),request.form.get('comment4'),
+#           request.form.get('comment5'),request.form.get('comment6'),
+#           request.form.get('comment7'),request.form.get('comment8'),
+#           request.form.get('comment9'),request.form.get('comment10'))
+#
+#     return render_template('preview.html')
 #
 #
 if __name__ == '__main__':
